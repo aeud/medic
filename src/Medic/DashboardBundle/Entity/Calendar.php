d39736e2ac97fbbd0ee1e055d5e5799a77153720
aeud <adrien.eudes@gmail.com>
2014-09-29 14:55:50 +0800

    Added User and Calendar entities to the model

---
+++ src/Medic/DashboardBundle/Entity/Calendar.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Medic\DashboardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Calendar
+ *
+ * @ORM\Table(name="calendar")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Calendar
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hash", type="string", length=8, unique=true)
+     */
+    private $hash;
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist() {
+        $this->setHash(hash('crc32b', uniqid('', true)));
+		$this->setCreatedAt(new \DateTime());
+		$this->setUpdatedAt(new \DateTime());
+    }
+	
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate() {
+		$this->setUpdatedAt(new \DateTime());
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Calendar
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Calendar
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set hash
+     *
+     * @param string $hash
+     * @return Calendar
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    /**
+     * Get hash
+     *
+     * @return string 
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+}
---
+++ src/Medic/DashboardBundle/Entity/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Medic\DashboardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="hash", type="string", length=8, unique=true)
+     */
+    private $hash;
+	
+	public function __construct()
+    {
+        parent::__construct();
+    }
+	
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist() {
+        $this->setHash(hash('crc32b', uniqid('', true)));
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hash
+     *
+     * @param string $hash
+     * @return User
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+
+        return $this;
+    }
+
+    /**
+     * Get hash
+     *
+     * @return string 
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+}
